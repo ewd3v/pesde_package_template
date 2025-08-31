10100044068ca13226c04db54b7b74ff29432656
ewd3v <74792428+ewd3v@users.noreply.github.com>
2025-08-31 16:20:29 +0200

    add runtime tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 pesde x ewdev/multitarget -- build --yes \
     --build-files src --output dist \
     --global RUNTIME --require-mode luau \
-    --luau --roblox
+    --luau --roblox # Runtimes to build for
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-luau tests/init.luau
+pesde x ewdev/multitarget -- build --yes \
+    --build-files src tests --output dist_test \
+    --global RUNTIME --require-mode luau \
+    --luau --lune # Runtimes to run tests on
+
+cd dist_test
+
+# TODO: Move different runtime tests into different workflow jobs
+
+if [ -d luau ]; then
+    echo "Running Luau tests..."
+    luau luau/tests/init.luau
+fi
+
+if [ -d lune ]; then
+    echo "Running Lune tests..."
+    lune run lune/tests/init.luau
+fi
+
+if [ -d roblox ]; then
+    echo "Running Roblox tests..."
+    echo "Tests currently don't run on Roblox"
+    exit 1
+fi
+
+echo "All runtime tests finished without issues!"
